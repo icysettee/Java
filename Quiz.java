fa633438fd80ccf00552d659ab4452e39ff17e4b
Aman Negi <aman363300@gmail.com>
2025-11-05 16:19:01 +0530

    Third project

---
+++ Quiz.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Quiz {     
+    public static void main(String [] args){
+
+        Scanner sc = new Scanner(System.in);
+        String[] que = {"Which of the following is not a primitive data type in Java?", "What is the default value of a boolean variable in Java?", "Which keyword is used to inherit a class in Java?", "Which method is the entry point of a Java program?"};
+        String[][] option ={
+                            {"1. double","2. int","3. boolean","4. String"},
+                            {"1. null","2. true","3. 0","4. false"},
+                            {"1. iherits","2. implement","3. super","4. extends"},
+                            {"1. run()","2. init()","3. main()","4. start()"}
+        };
+
+        int[] ans = {4,4,4,3};
+        int guess;
+        int marks=0;
+        
+        System.out.println("Welcome to the quix!");
+
+        for(int i = 0; i<que.length; i++){
+            System.out.println(que[i]);
+        
+                for (int k = 0; k<option[i].length; k++){
+                    
+                        System.out.println(option[i][k]);                        
+                }
+            guess = sc.nextInt();
+                        
+                if(guess == ans[i]){
+                    marks++;
+                }
+            
+        }
+
+        
+        System.out.printf("Your Scored %d Marks\nThat Means your Score is %.2f%%",marks,((double)marks/que.length)*100);
+        sc.close();
+    }
+    
+}
