6b55e56ac2677db30216aa9e76872267698ee5a1
Aman Negi <aman363300@gmail.com>
2025-11-06 01:59:19 +0530

    Optimised after tutorial

--- JanKenPo.java
+++ JanKenPo.java
@@ -14,34 +14,27 @@ public class JanKenPo {
         while(isRunning){
             System.out.println("THE game begins!!!");
             System.out.println("Enter your choice 1 -> Rock, 2 -> Paper, 3 -> Scissor");
+            String choice[]= {"Rock","Paper","Scissor"};
             userChoice = sc.nextInt();
+            System.out.print("user chose "+choice[userChoice - 1]);
             sc.nextLine();
             pcChoice = rd.nextInt(1,4);
+            System.out.println(" & pc chose "+choice[pcChoice - 1]);
             if(userChoice == pcChoice){
                 System.out.println("Draw!!!");
             }else if(userChoice==1 && pcChoice ==2){
-                System.out.println("User chose Rock & Pc chose Paper");
                 System.out.println("pc wins");
-            }else if(userChoice==2 && pcChoice ==1){
-                System.out.println("User chose Paper & Pc chose Rock");
-                System.out.println("user wins");
             }else if(userChoice==2 && pcChoice ==3){
-                System.out.println("User chose Paper & Pc chose Scissor");
                 System.out.println("pc wins");
-            }else if(userChoice==3 && pcChoice ==2){
-                System.out.println("User chose Scissor & Pc chose Paper");
-                System.out.println("user wins");
             }else if(userChoice==3 && pcChoice ==1){
-                System.out.println("User chose Scissor & Pc chose Rock");
                 System.out.println("pc wins");
-            }else if(userChoice==1 && pcChoice ==3){
-                System.out.println("User chose Rock & Pc chose Scissor");
+            }else{
                 System.out.println("user wins");
             }
 
-            System.out.printf("Wanna play again\nYes Or No\n");
-            retry = sc.next().charAt(0);
-            if(retry == 'y' || retry == 'Y'){
+            System.out.printf("Wanna play again?\nYes Or No\n");
+            retry = sc.next().toLowerCase().charAt(0);
+            if(retry == 'y'){
                 continue;
             }else{
                 break;
